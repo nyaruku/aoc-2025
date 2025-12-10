4ac2e12ff0d89577962ca3e5ec5779f064fe8099
nyaruku <38096703+nyaruku@users.noreply.github.com>
2025-12-10 23:52:18 +0100

    test

--- day1/1.php
+++ day1/1.php
@@ -1,14 +1,17 @@
 <?php
 
-$fileName = 'day1/input1.txt';
+$fileName = 'day1/input.txt';
 $dial = 50;
 $password = 0;
 
 $handle = fopen($fileName, "r");
 if ($handle) {
+    echo "-> " . $dial . "\n";
     while (($line = fgets($handle)) !== false) {
+        echo $line;
         $rotation = substr($line, 0, 1);
         $line = (int)ltrim($line, "RL");
+        $prevDial = $dial;
         switch($rotation) {
             case 'R':
                 $dial += $line;
@@ -17,10 +20,24 @@ if ($handle) {
                 $dial -= $line;
                 break;
         }
-        if($dial >= 100 || $dial < 0) {
-            $dial = $dial % 100;
-        }
-        if ($dial == 0) $password++;
+        echo "dial: " . $dial . "\n";
+        $fullCycles = 0;
+        $passwordIncrement = 0;
+        if ($dial > 99) {
+            $fullCycles = (int)($dial / 100);
+            $passwordIncrement = $fullCycles;
+            $dial -= ($fullCycles * 100);
+        } 
+        if ($dial < 0) {
+            $fullCycles = (int)(($dial * -1) / 100);
+            $passwordIncrement = (int)($dial / -100);
+            if ($prevDial != 0) $passwordIncrement++;
+            $dial = 100 + ($dial + ($fullCycles * 100));
+        } 
+        echo "dial: " . $dial . " - Cycles: " . $fullCycles . "\n";
+        $dial == 0 && $passwordIncrement == 0 ? $password++ : $password += $passwordIncrement;
+        echo "Password: " . $password . "\n";
+        
     }
     fclose($handle);
 }
